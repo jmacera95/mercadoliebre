3bd63535a4dcec7fb46d2d5e95ed92af3aa373c2
jmacera95 <julimacera@gmail.com>
2022-08-29 20:13:26 -0300

    hago cambios necesarios para publicar proyecto en heroku

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const path = require('path');
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
+
+//MIDDLEWARE
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('Server has started on port 3000.');
+app.set('puerto', process.env.PORT || 3030);
+
+app.listen(app.get('puerto'), () => {
+    console.log(`Server has started on port ${app.get('puerto')}.`);
 });
 
+//------ RUTAS ------
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/home.html'));
 });
